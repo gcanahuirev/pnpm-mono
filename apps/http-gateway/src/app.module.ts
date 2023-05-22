366d5c4eadbc20da407381c76b07a61c5644bd4f
Giuseppe <giuseppecv56@gmail.com>
2023-05-22 18:36:33 -0500

    feat: add allexceptionfilter to http-gateway app

--- apps/http-gateway/src/app.module.ts
+++ apps/http-gateway/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from '@pnpm-mono/common';
 import { ConfigModule, ConfigService } from '@pnpm-mono/config';
 import { LoggerModule } from '@pnpm-mono/logger';
 
@@ -8,7 +10,10 @@ import { AppService } from './app.service';
 @Module({
   imports: [ConfigModule, LoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+  ],
 })
 export class AppModule {
   static port: number;
